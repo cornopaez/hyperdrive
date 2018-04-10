e47f94df27cb5756b1ee939dbe1f653822a07e75
Dave York <dave.york@me.com>
2018-04-10 08:31:47 -0400

    fixing some errors and excluding the dotenv config variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const routes = require("./controllers/routes.js")
+const routes = require('./controllers/routes.js')
 const DIST_FOLDER = process.cwd() + '/dist'
 
 app.use(bodyParser.json()) // support json encoded bodies
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 app.use(express.static(DIST_FOLDER))
 
 // Main router -- ****OTHER ROUTERS NEED TO BE ABOVE THIS LINE!!!!******
-app.use("/", routes)
+app.use('/', routes)
 
 const port = process.env.PORT? process.env.PORT : 3000
 
