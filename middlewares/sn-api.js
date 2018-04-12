f7481dd8e9b34c3b264c0f487fca216252439a4b
Dave York <dave.york@me.com>
2018-04-12 09:55:18 -0400

    Added documentation to getUserDetails and moved the JSON parsing of its response.

--- middlewares/sn-api.js
+++ middlewares/sn-api.js
@@ -62,6 +62,13 @@ function search(search_string) {
   })
 }
 
+/**
+  This function searches the configured ServiceNow endpoint. It returns a Promise that resolves the data as plain text or provides
+  the error occured.
+
+  @param email_address - The email address of the person being looked for
+  @return - Promise. Resolves to JSON object containing the user's information
+*/
 function getUserDetails(email_address) {
 
 
