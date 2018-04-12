0816b662606a33b7d877bd11b8e7578d05db6ca1
Dave York <dave.york@me.com>
2018-04-12 10:05:01 -0400

    adding eslint and linter rules

--- middlewares/intent-processing.js
+++ middlewares/intent-processing.js
@@ -4,80 +4,80 @@ const getIncidentDetails = require('./sn-api.js').getIncidentDetails
 const closeIncident = require('./sn-api.js').closeIncident
 
 module.exports = {
-	processIntent: processIntent
+    processIntent: processIntent
 }
 
 function processIntent(request_body) {
 
-	return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-		switch (request_body.result.action) {
+        switch (request_body.result.action) {
 
-			case 'search_kb':
-				search(request_body.result.action) //Needs to change when we know where the information is coming from
-				.then(success => {
-					console.log('search_kb success!')
-					resolve(success)
-				})
-				.catch(error => {
-					console.log('search_kb error!')
-					reject(error)
-				})
-				break
+        case 'search_kb':
+            search(request_body.result.action) //Needs to change when we know where the information is coming from
+                .then(success => {
+                    console.log('search_kb success!')
+                    resolve(success)
+                })
+                .catch(error => {
+                    console.log('search_kb error!')
+                    reject(error)
+                })
+            break
 
-			case 'search_user':
-				getUserDetails('mark.ludwig@pnc.com') //Needs to change when we know where the information is coming from
-				.then(success => {
-					console.log('search_user success!')
-					resolve(success)
-				})
-				.catch(error => {
-					console.log('search_user error!')
-					reject(error)
-				})
-				break
+        case 'search_user':
+            getUserDetails('mark.ludwig@pnc.com') //Needs to change when we know where the information is coming from
+                .then(success => {
+                    console.log('search_user success!')
+                    resolve(success)
+                })
+                .catch(error => {
+                    console.log('search_user error!')
+                    reject(error)
+                })
+            break
 
-			case 'create_incident':
-				break
+        case 'create_incident':
+            break
 
-			case 'search_incident':
-				getIncidentDetails(request_body.resolvedQuery) //Needs to change when we know where the information is coming from
-				.then(success => {
-					console.log('search_incident success!')
-					resolve(success)
-				})
-				.catch(error => {
-					console.log('search_incident error!')
-					reject(error)
-				})
-				break
+        case 'search_incident':
+            getIncidentDetails(request_body.resolvedQuery) //Needs to change when we know where the information is coming from
+                .then(success => {
+                    console.log('search_incident success!')
+                    resolve(success)
+                })
+                .catch(error => {
+                    console.log('search_incident error!')
+                    reject(error)
+                })
+            break
 
-			case 'close_incident':
-				// Local vars
-				// These are based on a sample call from API.AI
-				var case_number = request_body.result.parameters.case
-				var close_notes = request_body.result.parameters.notes
+        case 'close_incident':
+            // Local vars
+            // These are based on a sample call from API.AI
+            var case_number = request_body.result.parameters.case
+            var close_notes = request_body.result.parameters.notes
 
-				closeIncident(case_number, close_notes)
-				.then(success => {
-					console.log('close_incident success!')
-					resolve(success)
-				})
-				.catch(error => {
-					console.log('close_incident error!')
-					reject(error)
-				})
-				break
+            closeIncident(case_number, close_notes)
+                .then(success => {
+                    console.log('close_incident success!')
+                    resolve(success)
+                })
+                .catch(error => {
+                    console.log('close_incident error!')
+                    reject(error)
+                })
+            break
 
-			default:
-				var response = {
-					statusCode: 500,
-					body: {
-						message: 'Error: Unknown Intent Action.'
-					}
-				}
-				reject(response)
-				break
-		}
-	})
+        default:
+            var response = {
+                statusCode: 500,
+                body: {
+                    message: 'Error: Unknown Intent Action.'
+                }
+            }
+            reject(response)
+            break
+        }
+    })
 }
--- middlewares/sn-api.js
+++ middlewares/sn-api.js
@@ -2,21 +2,21 @@ const request = require('request')
 const btoa = require('btoa')
 const argv = require('yargs').argv
 const urlencode = require('urlencode')
-const baseurl = "https://gpietro3demo.service-now.com"
+const baseurl = 'https://gpietro3demo.service-now.com'
 const dotenv = require('dotenv').config()
 const searchuri = '/api/now/table/kb_knowledge'
 const userUri = '/api/now/table/sys_user'
 const incidentUri = '/api/now/table/incident'
-const kburi = `/nav_to.do?uri=%2Fkb_view.do%3Fsys_kb_id%3D`
+const kburi = '/nav_to.do?uri=%2Fkb_view.do%3Fsys_kb_id%3D'
 const auth = 'Basic ' + btoa(`${argv.username||process.env.username}`+':'+`${argv.password||process.env.password}`)
 
 // module.exports.search = search
 // module.exports.getUserDetails = getUserDetails
 module.exports = {
-  search: search,
-  getUserDetails: getUserDetails,
-  getIncidentDetails: getIncidentDetails,
-  closeIncident: closeIncident
+    search: search,
+    getUserDetails: getUserDetails,
+    getIncidentDetails: getIncidentDetails,
+    closeIncident: closeIncident
 }
 
 /**
@@ -27,39 +27,39 @@ module.exports = {
   @return - Promise. Resolves to plain text from relevant articles returned.
 */
 function search(search_string) {
+    
+    return new Promise((resolve, reject) =>{
 
-  return new Promise((resolve, reject) =>{
+        var search_query = '123TEXTQUERY321=' + urlencode(search_string) // Beginning of string necessary for query to work
 
-    var search_query = '123TEXTQUERY321=' + urlencode(search_string) // Beginning of string necessary for query to work
-
-    var options = { 
-      method: 'GET',
-      uri: baseurl + searchuri,
-      qs: { 
-        sysparm_query: search_query,
-        sysparm_limit: '3',
-        workflow_state: 'published' 
-      },
-      json: true,
-      headers: {
-        'Cache-Control': 'no-cache',
-        Authorization: auth 
-      } 
-    }
-
-    request(options, (error, response, body) => {
-      var search_results = ""
-      if (!error && response.statusCode == 200) {
-        for (let result of body.result) {
-          var short_description = JSON.stringify(result.short_description)
-          search_results = search_results + ` ${short_description} \n `+ baseurl + kburi + result.sys_id + "\n"
+        var options = { 
+            method: 'GET',
+            uri: baseurl + searchuri,
+            qs: { 
+                sysparm_query: search_query,
+                sysparm_limit: '3',
+                workflow_state: 'published' 
+            },
+            json: true,
+            headers: {
+                'Cache-Control': 'no-cache',
+                Authorization: auth 
+            } 
         }
-      } else {
-        reject(response)
-      }
-      resolve(search_results)
+
+        request(options, (error, response, body) => {
+            var search_results = ''
+            if (!error && response.statusCode == 200) {
+                for (let result of body.result) {
+                    var short_description = JSON.stringify(result.short_description)
+                    search_results = search_results + ` ${short_description} \n `+ baseurl + kburi + result.sys_id + '\n'
+                }
+            } else {
+                reject(response)
+            }
+            resolve(search_results)
+        })
     })
-  })
 }
 
 /**
@@ -72,30 +72,30 @@ function search(search_string) {
 function getUserDetails(email_address) {
 
 
-  return new Promise((resolve, reject) => {
-    var options = { 
-      method: 'GET',
-      uri: baseurl + userUri,
-      qs: { 
-        sysparm_query: 'email=' + email_address,
-        sysparm_limit: '1' //This can be changed to suit our needs 
-      },
-      headers: { 
-        'Cache-Control': 'no-cache',
-        Authorization: auth 
-      } 
-    };
+    return new Promise((resolve, reject) => {
+        var options = { 
+            method: 'GET',
+            uri: baseurl + userUri,
+            qs: { 
+                sysparm_query: 'email=' + email_address,
+                sysparm_limit: '1' //This can be changed to suit our needs 
+            },
+            headers: { 
+                'Cache-Control': 'no-cache',
+                Authorization: auth 
+            } 
+        };
 
-    request(options, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        resolve(JSON.parse(body))
-      } else {
-        console.log('request error!')
-        // console.log(response)
-        reject(response)
-      }
+        request(options, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                resolve(JSON.parse(body))
+            } else {
+                console.log('request error!')
+                // console.log(response)
+                reject(response)
+            }
+        })
     })
-  })
 }
 
 /**
@@ -107,32 +107,32 @@ function getUserDetails(email_address) {
 */
 function getIncidentDetails(incident_number) {
 
-  return new Promise((resolve, reject) => {
-    var options = { 
-      method: 'GET',
-      url: baseurl + incidentUri,
-      qs: { 
-        sysparm_query: 'number=' + incident_number, 
-        sysparm_limit: '1' 
-      },
-      headers: {
-        'Cache-Control': 'no-cache',
-        Accept: 'application/json',
-        'Content-Type': 'application/json' ,
-        Authorization: auth
-      } 
-    }
+    return new Promise((resolve, reject) => {
+        var options = { 
+            method: 'GET',
+            url: baseurl + incidentUri,
+            qs: { 
+                sysparm_query: 'number=' + incident_number, 
+                sysparm_limit: '1' 
+            },
+            headers: {
+                'Cache-Control': 'no-cache',
+                Accept: 'application/json',
+                'Content-Type': 'application/json' ,
+                Authorization: auth
+            } 
+        }
 
-    request(options, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        resolve(JSON.parse(body))
-      } else {
-        console.log('request error!')
-        // console.log(response)
-        reject(response)
-      }
+        request(options, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                resolve(JSON.parse(body))
+            } else {
+                console.log('request error!')
+                // console.log(response)
+                reject(response)
+            }
+        })
     })
-  })
 }
 
 /**
@@ -147,46 +147,46 @@ function getIncidentDetails(incident_number) {
 */
 function closeIncident(incident_number, close_notes, close_code = 'Closed/Resolved by Caller', user_id = '9ee1b13dc6112271007f9d0efdb69cd0') {
 
-  return new Promise((resolve, reject) => {
-    getIncidentDetails(incident_number)
-    .then(success => {
-      var options = { 
-          method: 'PATCH',
-          url: baseurl + incidentUri + '/' + success.result[0].sys_id,
-          headers: { 
-             'Cache-Control': 'no-cache',
-             Authorization: auth,
-             Accept: 'application/json',
-             'Content-Type': 'application/json' 
-          },
-          body: { 
-            close_notes: close_notes,
-            closed_by: user_id,
-            close_code: close_code,
-            closed_at: Date() 
-          },
-          json: true 
-        }
+    return new Promise((resolve, reject) => {
+        getIncidentDetails(incident_number)
+            .then(success => {
+                var options = { 
+                    method: 'PATCH',
+                    url: baseurl + incidentUri + '/' + success.result[0].sys_id,
+                    headers: { 
+                        'Cache-Control': 'no-cache',
+                        Authorization: auth,
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json' 
+                    },
+                    body: { 
+                        close_notes: close_notes,
+                        closed_by: user_id,
+                        close_code: close_code,
+                        closed_at: Date() 
+                    },
+                    json: true 
+                }
 
-      request(options, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-          resolve(body) // This response is already JSON. Parsing it will give an error.
-        } else {
-          console.log('request error!')
-          // console.log(response)
-          reject(response)
-        }
-      })        
+                request(options, (error, response, body) => {
+                    if (!error && response.statusCode == 200) {
+                        resolve(body) // This response is already JSON. Parsing it will give an error.
+                    } else {
+                        console.log('request error!')
+                        // console.log(response)
+                        reject(response)
+                    }
+                })        
+            })
+            .catch(error => {
+            // If incident number comes up empty, handle error
+                var response = {
+                    statusCode: 500,
+                    body: {
+                        message: 'Error: Could not close incident. Incident number is invalid. Please try again.'
+                    }
+                }
+                reject(response)
+            })
     })
-    .catch(error => {
-      // If incident number comes up empty, handle error
-      var response = {
-            statusCode: 500,
-            body: {
-              message: 'Error: Could not close incident. Incident number is invalid. Please try again.'
-            }
-          }
-      reject(response)
-    })
-  })
 }
